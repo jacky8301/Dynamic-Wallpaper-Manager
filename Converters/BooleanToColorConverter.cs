df7cf3e22991f7d12d0ec995c411c0dd92ecfd12
jacky8301 <zhranjie@163.com>
2026-01-11 11:11:22 +0800

    add BooleanToColorConverter.cs

---
+++ Converters/BooleanToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WallpaperEngine.Converters
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public System.Windows.Media.Brush TrueBrush { get; set; } = System.Windows.Media.Brushes.Green;
+        public System.Windows.Media.Brush FalseBrush { get; set; } = System.Windows.Media.Brushes.Gray;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? TrueBrush : FalseBrush;
+            return FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
