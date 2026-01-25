242d698e29c859de8a3fca124305197471f018a7
jacky8301 <zhranjie@163.com>
2026-01-25 12:54:16 +0800

    增加图片缓存机制

--- Converters/ImagePathToSourceConverter.cs
+++ Converters/ImagePathToSourceConverter.cs
@@ -1,17 +1,26 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 using System.IO;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 using WallpaperEngine.Services;
 
 namespace WallpaperEngine.Converters {
     public class ImagePathToSourceConverter : IValueConverter {
+        private static readonly ConcurrentDictionary<string, BitmapImage> _cache =
+            new ConcurrentDictionary<string, BitmapImage>(StringComparer.OrdinalIgnoreCase);
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string imagePath = value as string;
             if (string.IsNullOrEmpty(imagePath) || !File.Exists(imagePath)) {
                 return null;
             }
-            return ImageLoader.LoadImage(imagePath);
+            if (_cache.TryGetValue(imagePath, out var cachedImage)) {
+                return cachedImage;
+            }
+            var bitmap = ImageLoader.LoadImage(imagePath);
+            _cache[imagePath] = bitmap;
+            return bitmap;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
