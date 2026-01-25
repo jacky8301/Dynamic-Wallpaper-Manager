66a7d853b22c75d11d2fb086c93387308cbfbd1c
jacky8301 <zhranjie@163.com>
2026-01-25 16:43:19 +0800

    去掉注释代码

--- Models/WallpaperItem.cs
+++ Models/WallpaperItem.cs
@@ -7,13 +7,11 @@ namespace WallpaperEngine.Models {
         public string FolderPath { get; set; } = string.Empty;
         public string FolderName => Path.GetFileName(FolderPath);
         public WallpaperProject Project { get; set; } = new WallpaperProject();
-        //public bool IsFavorite { get; set; }
         public string Category { get; set; } = "未分类";
         public DateTime AddedDate { get; set; } = DateTime.Now;
         public string PreviewImagePath => Path.Combine(FolderPath, Project.Preview);
         public string ContentPath => Path.Combine(FolderPath, Project.File);
         public bool IsSelected { get; set; }
-        //public string FavoritedDate { get; set; } = DateTime.Now.ToString("O");
         public string LastUpdated { get; set; } = DateTime.Now.ToString("O");
         // 一个便利属性，用于判断是否为新添加的壁纸（非数据库中存在）
         [ObservableProperty]
