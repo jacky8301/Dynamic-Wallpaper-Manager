e27769f8bcda8912dd776ebd3a81a8b518f2a6c0
jacky8301 <zhranjie@163.com>
2026-02-01 22:30:40 +0800

    标题字段编辑功能开发

---
+++ Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WallpaperEngine.Converters {
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InverseBooleanToVisibilityConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 将 bool 值取反
+           if (value is bool b)
+                return b ? Visibility.Collapsed : Visibility.Visible;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 若需双向绑定，可在此实现反向逻辑；否则抛出异常
+            throw new NotSupportedException();
+        }
+    }
+}
